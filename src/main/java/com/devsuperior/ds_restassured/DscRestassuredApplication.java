9018465a8d3aec7156e8a3bb02f2f633d7bd0d38
Miguelito2926 <juniorprol@hotmail.com>
2025-11-21 22:49:21 -0300

    criando projeto

---
+++ src/main/java/com/devsuperior/ds_restassured/DscRestassuredApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.ds_restassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscRestassuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscRestassuredApplication.class, args);
+	}
+
+}
